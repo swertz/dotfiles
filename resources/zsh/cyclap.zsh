74b2cbe5802cff17d77ef5d7989c405fe796915c
Sebastien Wertz <sebastien.wertz@uclouvain.be>
2018-07-31 15:23:37 +0200

    changes for new cyclap install

--- resources/zsh/cyclap.zsh
+++ resources/zsh/cyclap.zsh
@@ -6,7 +6,7 @@ fi
 
 # User specific aliases and functions
 
-alias r6='source /home/swertz/.local/soft/root/builddir/bin/thisroot.sh'
+alias r6='source /home/swertz/.local/test/root/build_dir/bin/thisroot.sh'
 alias don='xrandr --output DP-1-1 --auto --right-of eDP-1'
 alias doff='xrandr --output DP-1-1 --off'
 alias pip_upgrade='sudo pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs sudo pip install -U'
@@ -14,7 +14,8 @@ alias pip_upgrade='sudo pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | x
 export GTK2_RC_FILES=$HOME/.gtkrc-2.0
 export VIDYO_AUDIO_FRAMEWORK=ALSA
 export PYTHONSTARTUP=~/.pythonrc
-export TEXINPUTS=$TEXINPUTS:/home/swertz/Downloads/mtheme/:/home/swertz/Downloads/hepnames/:/home/swertz/Downloads/hepparticles/:
+export SSH_ASKPASS=ksshaskpass
+export KRB5_CONFIG=$HOME/.dotfiles/resources/krb.conf
 
 alias konq='konqueror --profile filemanagement&'
 
@@ -23,6 +24,10 @@ case "$TERM" in
     xterm-color) color_prompt=yes;;
 esac
 
-ssh-add $HOME/.ssh/id_rsa.ceci </dev/null > /dev/null
-ssh-add $HOME/.ssh/id_rsa </dev/null > /dev/null
+if [[ -d "$HOME/.local/bin" ]]; then
+    PATH="$HOME/.local/bin:$PATH"
+fi
+
+#ssh-add $HOME/.ssh/id_rsa.ceci </dev/null > /dev/null
+#ssh-add $HOME/.ssh/id_rsa </dev/null > /dev/null
 
