7e1f0969859898aeffbdad85ab21a2cca33e0549
Sebastien Wertz <sebastien.wertz@cern.ch>
2018-10-01 12:00:03 +0200

    further changes for thinkpad

--- resources/zsh/cyclap.zsh
+++ resources/zsh/cyclap.zsh
@@ -7,27 +7,16 @@ fi
 # User specific aliases and functions
 
 alias r6='source /home/swertz/.local/test/root/build_dir/bin/thisroot.sh'
-alias don='xrandr --output DP-1-1 --auto --right-of eDP-1'
-alias doff='xrandr --output DP-1-1 --off'
+alias don='xrandr --output DP-1 --auto --above eDP-1'
+alias doff='xrandr --output DP-1 --off'
 alias pip_upgrade='sudo pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs sudo pip install -U'
 
 export GTK2_RC_FILES=$HOME/.gtkrc-2.0
-export VIDYO_AUDIO_FRAMEWORK=ALSA
 export PYTHONSTARTUP=~/.pythonrc
 export SSH_ASKPASS=ksshaskpass
 export KRB5_CONFIG=$HOME/.dotfiles/resources/krb.conf
 
-alias konq='konqueror --profile filemanagement&'
-
-# set a fancy prompt (non-color, unless we know we "want" color)
-case "$TERM" in
-    xterm-color) color_prompt=yes;;
-esac
-
 if [[ -d "$HOME/.local/bin" ]]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
-
-#ssh-add $HOME/.ssh/id_rsa.ceci </dev/null > /dev/null
-#ssh-add $HOME/.ssh/id_rsa </dev/null > /dev/null
 
