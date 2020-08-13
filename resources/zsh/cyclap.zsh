e6cce5c92edcbf8e04a832541da3370566315200
Sebastien Wertz <sebastien.wertz@cern.ch>
2020-08-13 14:29:43 +0200

    ssh and ingrid adjustments

--- resources/zsh/cyclap.zsh
+++ resources/zsh/cyclap.zsh
@@ -6,18 +6,19 @@ fi
 
 # User specific aliases and functions
 
-alias r6='source /home/swertz/.local/test/root-6.18.04/builddir/bin/thisroot.sh'
+alias r6='source /home/swertz/.local/test/root-6.22.0/bin/thisroot.sh'
 alias don='xrandr --output DP-1 --auto --right-of eDP-1'
 alias doff='xrandr --output DP-1 --off'
 alias pip_upgrade='pip3 freeze --local | grep -v ^-e | cut -d = -f 1  | xargs pip3 install -U'
 
 export GTK2_RC_FILES=$HOME/.gtkrc-2.0
 export SSH_ASKPASS=ksshaskpass
-export KRB5_CONFIG=$HOME/.dotfiles/resources/krb.conf
 
 if [[ -d "$HOME/.local/bin" ]]; then
     PATH="$HOME/.local/bin:$PATH"
 fi
+
+source /opt/conda/etc/profile.d/conda.sh
 
 function mountPSI() {
     sshfs -o follow_symlinks,reconnect,ServerAliveInterval=15,ServerAliveCountMax=3 swertz@t3ui03.psi.ch:/t3home/swertz/ /media/psi
