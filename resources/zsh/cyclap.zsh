a4cdd8eb2cb96258d57f9cba4ae43d926524fff7
Sebastien Wertz <sebastien.wertz@cern.ch>
2022-03-07 11:21:20 +0100

    miniconda on thinkpad

--- resources/zsh/cyclap.zsh
+++ resources/zsh/cyclap.zsh
@@ -34,3 +34,19 @@ function unmountAll() {
     fusermount -uz /media/psi
     fusermount -uz /media/lxplus
 }
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/swertz/.miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/swertz/.miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/swertz/.miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/swertz/.miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+
